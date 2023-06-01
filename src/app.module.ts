2a35f6ed7b0f161d26b59be13ad732a1dcaa1dd7
andrsand <angorko@list.ru>
2023-06-01 23:41:52 +0300

    Creating the controller products.controller

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ProductsController } from './products/products.controller';
 
-@Module({
+@Module({ // @Module - это декоратор
   imports: [],
-  controllers: [AppController],
+  controllers: [AppController, ProductsController], // AppController из app.controller.ts
   providers: [AppService],
 })
 export class AppModule {}
