a7b0ed2601091931eb279c8afea26792583f809d
andrsand <angorko@list.ru>
2023-06-03 23:49:28 +0300

    Create dto folder and file with create-product.dto.ts method

--- src/products/products.controller.ts
+++ src/products/products.controller.ts
@@ -1,4 +1,5 @@
-import { Controller, Get, Param } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Post, Put } from '@nestjs/common';
+import { CreateProductDto } from './dto/create-product.dto';
 
 @Controller('products')
 export class ProductsController{
@@ -11,6 +12,21 @@ export class ProductsController{
     @Get(':id')
     getOne(@Param('id') id: string):string {
         return 'getOne' + id
+    }
+
+    @Post()
+    create(@Body() CreateProductDto: CreateProductDto): string {
+        return `Title: ${CreateProductDto.title} Price: ${CreateProductDto.price}`
+    }
+
+    @Delete()
+    remove() {
+        
+    }
+
+    @Put()
+    update() {
+        
     }
 }
     
