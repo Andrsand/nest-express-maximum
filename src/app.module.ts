975f84868a83168654d3065de1d14a2739b08b2a
andrsand <angorko@list.ru>
2023-06-07 11:55:40 +0300

    Example of creating services

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ProductsController } from './products/products.controller';
+import { ProductsService } from './products/products.service';
 
 @Module({ // @Module - это декоратор
   imports: [],
   controllers: [AppController, ProductsController], // AppController из app.controller.ts
-  providers: [AppService],
+  providers: [AppService, ProductsService],
 })
 export class AppModule {}
--- src/products/products.controller.ts
+++ src/products/products.controller.ts
@@ -2,26 +2,38 @@ import { Body, Controller, Delete, Get, Header, HttpCode, HttpStatus, Param, Pos
 import { CreateProductDto } from './dto/create-product.dto';
 import { UpdateProductDto } from './dto/update-product.dto';
 import { url } from 'inspector';
+// import { Response, Request } from 'express';
+import { ProductsService } from './products.service';
 
 @Controller('products')
 export class ProductsController{
+
+    constructor(private readonly productsService: ProductsService) {
+
+    }
+    
+//     @Get()
+//    // @Redirect('https://google.com', 301)
+//     getAll(@Req() req: Request, @Res() res: Response):string {
+//         res.status(201).end('Poka')
+//         return 'getAll'
+    //     }
     
     @Get()
-   // @Redirect('https://google.com', 301)
-    getAll():string {
-        return'getAll'
+    getAll() {
+        return this.productsService.getAll()
     }
     
     @Get(':id')
-    getOne(@Param('id') id: string):string {
-        return 'getOne' + id
+    getOne(@Param('id') id: string) {
+        return this.productsService.getById(id)
     }
 
     @Post()
     @HttpCode(HttpStatus.CREATED)
     @Header('cache-Control', 'none')
-    create(@Body() CreateProductDto: CreateProductDto): string {
-        return `Title: ${CreateProductDto.title} Price: ${CreateProductDto.price}`
+    create(@Body() CreateProductDto: CreateProductDto) {
+        return this.productsService.create(CreateProductDto)
     }
 
     @Delete(':id')
