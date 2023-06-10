20d29206418aa10bd76369703d5e32693f42219a
andrsand <angorko@list.ru>
2023-06-10 23:40:05 +0300

    Removing ProductsService and ProductsController from app.module.ts and adding to the imports - ProductsMosule array

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,10 +3,11 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ProductsController } from './products/products.controller';
 import { ProductsService } from './products/products.service';
+import { ProductsModule } from './products/products.module';
 
 @Module({ // @Module - это декоратор
-  imports: [],
-  controllers: [AppController, ProductsController], // AppController из app.controller.ts
-  providers: [AppService, ProductsService],
+  imports: [ProductsModule],
+  controllers: [AppController], // AppController из app.controller.ts
+  providers: [AppService],
 })
 export class AppModule {}
